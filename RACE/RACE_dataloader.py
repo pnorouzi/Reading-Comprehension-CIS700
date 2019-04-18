f2c3c68ebf80c30a565f1645f7690776b579716d
Leonardo Murri <murri@seas.upenn.edu>
2019-04-18 15:56:44 -0400

    Added embedding RACE transform

--- RACE/RACE_dataloader.py
+++ RACE/RACE_dataloader.py
@@ -1,21 +1,39 @@
 import os
+import pickle
+import bcolz
+import numpy as np
+from ast import literal_eval
 
 import torch
 import torchvision
 import torchvision.transforms as transforms
 import pandas as pd
 from torch.utils import data
-from ast import literal_eval
+
+from RACE_transforms import RACE_GloVe_Transform
+
+'''
+HOW TO USE:
+$ from RACE_dataloder import get_dataloaders(transforms = None, batch_size = 32)
+This returns train, dev, test dataloaders with appropriate transfroms on the RACE dataset
+
+'''
+
 
 class RACE_Dataset(data.Dataset):
-    def __init__(self, root = 'RACE_Data/', split = 0, transforms = None):
-        # root (str): Dataset folder path
-        # split (int): 0-1-2 for train-dev-test dataset
-        # transforms (transform): pytorch input transform
+    def __init__(self, root = 'RACE_Data/', split = 0, transformer = None):
+        """ Intializie a RACE Dataset
 
+        :param root: Dataset folder path
+        :param split: 0-1-2 for train-dev-test dataset
+        :param transforms: pytorch input transform
+        :param glove: GloVe emdedding dictionary
+        :return RACE_Dataset object
+        """
         self.split = split
-        self.transforms = transforms
+        self.transformer = transformer
 
+        # Extract file paths for dataset
         self.files = []
         paths = ['train/', 'dev/', 'test/']
         path = root + paths[split]
@@ -23,45 +41,48 @@ class RACE_Dataset(data.Dataset):
             for filename in filenames:
                 self.files.append(os.path.join(root,filename))
 
+
     def __len__(self):
         return len(self.files)
 
     def __getitem__(self, index):
         # Input: index(int)
-        # Output: Paragraph (str), questions (list[str]), answer (str), options (list[str])
+        # Output: Paragraph, question, answer, options (list[])
 
         # Open file at index
         file = self.files[index]
         df = pd.read_csv(file).iloc[0]
 
-        # Extract text answer from list of options
-        letter_to_index = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6}
-        # print(df['answers'])
-        # print(letter_to_index[df['answers']])
-        # print(literal_eval(df['options']))
-        answer = literal_eval(df['options'])[letter_to_index[df['answers']]]
-
         # Extract data
         article = df['article']
         questions = df['questions']
-        options = df['options']
+        options = literal_eval(df['options'])
+        answer = options[ord(df['answers']) - 65]
 
-        # Perform data transfrom if avaliable
-        if self.transforms:
-            article = self.transforms(article)
-            questions = self.transforms(questions)
-            options = self.transforms(options)
-            answer = self.transforms(answer)
+        # Perform data transfrom
+        if self.transformer:
+            article = self.transformer.transform(article)
+            question = self.transformer.transform(questions)
+            options = [self.transformer.transform(option) for option in options]
+            answer = self.transformer.transform(answer)
 
-        return article, questions, answer, options
+        return article, question, answer, options
 
-def get_dataloaders(transforms = None, batch_size = 32):
-    # Input: split (int) = 0 for train, 1 for dev, 2 for test
-    # Ouput: train, dev, test dataloaders
 
-    train_dataset = RACE_Dataset(split = 0, transforms = transforms)
-    dev_dataset = RACE_Dataset(split = 1, transforms = transforms)
-    test_dataset = RACE_Dataset(split = 2, transforms = transforms)
+def get_dataloaders(batch_size = 32, transformer_type = 'embded_text_and_pad'):
+    """Creates RACE train, dev and test dataloaders
+
+    :param split: int 0 to 2 representing train, dev and test repesctively
+    :param batch_size: size of dataloader batch
+    :parm glove_embed: bool that specifies if you want your strings to be glove embdded
+    :return train, dev, test Pytorch dataloaders
+    """
+
+    transformer = RACE_GloVe_Transform(transformer_type = transformer_type)
+
+    train_dataset = RACE_Dataset(split = 0, transformer = transformer)
+    dev_dataset = RACE_Dataset(split = 1, transformer = transformer)
+    test_dataset = RACE_Dataset(split = 2, transformer = transformer)
 
     train_loader = data.DataLoader(train_dataset, batch_size = batch_size, shuffle = True)
     dev_loader = data.DataLoader(dev_dataset, batch_size = batch_size, shuffle = True)
@@ -69,11 +90,12 @@ def get_dataloaders(transforms = None, batch_size = 32):
 
     return train_loader, dev_loader, test_loader
 
-# if __name__ == '__main__':
-#     train, _, _ = get_dataloaders()
-#     for x,y,z,w in train:
-#         print(x[0])
-#         print(y[0])
-#         print(z[0])
-#         #print(w[0])
-#         break
+
+if __name__ == '__main__':
+
+    transformer = RACE_GloVe_Transform(transformer_type = 'embded_text_and_pad')
+    train_loader, dev_loader, test_loader = get_dataloaders(transformer = transformer)
+
+    for (article, questions, answer, options) in train_loader:
+
+        break
---
+++ RACE/RACE_transforms.py
@@ -0,0 +1,81 @@
+import os
+import pickle
+import bcolz
+import numpy as np
+import torch
+
+'''
+Ask me if you got question why we need a class to do this is a pytorch thing
+
+transformer_types:
+:pad
+:average
+
+'''
+
+class RACE_GloVe_Transform:
+    def __init__(self, transformer_type = 'embded_text_and_pad', glove_size = 300):
+        glove_path = '../GloVe/glove.6B'
+        vectors = bcolz.open(f'{glove_path}/6B.{str(glove_size)}.dat')[:]
+        words = pickle.load(open(f'{glove_path}/6B.{str(glove_size)}_words.pkl', 'rb'))
+        word2idx = pickle.load(open(f'{glove_path}/6B.{str(glove_size)}_idx.pkl', 'rb'))
+
+        self.glove = {w: vectors[word2idx[w]] for w in words}
+        self.glove_size = glove_size
+
+        if transformer_type == 'pad':
+            self.transform = self.emebed_text_and_pad
+        elif transformer_type == 'average':
+            self.transform = self.embed_text_and_average
+
+    def emebed_text_and_pad(self, string, max_length = 4000):
+        """ GloVe Word Embedding
+        Takes in a string however long and converts into word embedded np.array of
+        size (glove-size)x(max_length) that is each column is a word. Moroever,
+        it pads the with zeros to max_length size. Truncates if too long.
+
+        :param string: input string you want to convert to vector representation
+        :param max_length: size of dim 1 in output
+        :return torch tensor of size (glove-size)x(max_length)
+        """
+
+        string = string.replace('.', ' . ')
+        string = string.replace('"', ' " ')
+        string = string.replace('\n', ' | ')
+        string = string.replace('?', ' ? ')
+        string = string.replace('!', ' ! ')
+
+        words = string.split(' ')
+        num_words = len(words)
+        glove_size = self.glove_size
+
+        #print(words)
+        output = np.zeros((glove_size,max_length))
+        for index, word in enumerate(words):
+            # Convert word to vector
+            if word in self.glove:
+                output[:, index] = self.glove[word.lower()]
+            else:
+                #print(word)
+                pass
+            # Truncates if there are more words than max_length
+            if index == max_length - 1:
+                break
+
+        return torch.from_numpy(output)
+
+    def embed_text_and_average(self, string):
+        """ GloVe Average Word Embdeding
+        Takes in a string and convertes it to embded vectors matrix
+        and then takes the average
+
+        :param string: input string
+        :return torch tensor of size glove_size
+        """
+
+        output = self.emebed_text_and_pad(string, max_length = len(string)/2)
+
+        # Average word vectors
+        output = np.mean(output, axis = 1)
+
+        return torch.from_numpy(output)
