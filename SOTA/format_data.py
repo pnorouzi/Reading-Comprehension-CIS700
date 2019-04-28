547d174da0db2a1c9c0fae5993cfd0da913b445b
Sam Oshay <39424711+soshay@users.noreply.github.com>
2019-04-28 18:24:34 -0400

    Create format_data.py

---
+++ SOTA/format_data.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""This class processing the RACE dataset into the proper
+format for accompanying Dual Co-Matching Network architecture.
+
+Implementation by Samuel Oshay for University of Pennsylvania's 
+CIS-700 Deep Learning, Spring 2019.
+
+Teammates: Leonardo Murri, Dewang Sultania, Peyman Norouzi."""
+
+# ==========================================================
+# IMPORTS
+# ==========================================================
+
